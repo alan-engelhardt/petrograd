bd7cb63ce5635bd85e88591e18ab3916b120efb7
Alan Engelhardt <ale@kea.dk>
2017-08-10 09:39:30 +0200

    Add a (rudimentary) show details function

--- js/petrograd.js
+++ js/petrograd.js
@@ -1,33 +1,54 @@
 let main = document.querySelector("main");
 let categories_link = "http://kea-alt-del.dk/t5/api/categories";
+let product_link = "http://kea-alt-del.dk/t5/api/product?id=";
 
-function loadJSON(link, process, daddy=null){
-	fetch(link).then(e=>e.json()).then(data=>process(data, daddy));
+function loadJSON(link, process, container=null){
+	fetch(link).then(e=>e.json()).then(data=>process(data, container));
 }
 
 function loopCategories(data){
 	data.forEach(category => {
 		let clone = document.querySelector(".category").content.cloneNode(true);
 		let link = "http://kea-alt-del.dk/t5/api/productlist?category=" + category;
-		let parent = document.createElement('section');
+		let productContainer = document.createElement('section');
+		productContainer.classList.add("productContainer");
 		clone.querySelector("h1").textContent = category;
 		main.appendChild(clone);
-		main.appendChild(parent);
-		loadJSON(link, showList, parent);
+		main.appendChild(productContainer);
+		loadJSON(link, showList, productContainer);
 	});
 }
 
-function showList(data, daddy){
+function showList(data, container){
 	data.forEach(product => {
 		let clone = document.querySelector(".product").content.cloneNode(true);
-		clone.querySelector(".name").textContent = product.name;
-		clone.querySelector(".product-small-img").src = "http://kea-alt-del.dk/t5/site/imgs/small/" + product.image + "-sm.jpg";
-		clone.querySelector(".product-small-img").alt = product.name;
-		clone.querySelector(".price").textContent = product.price;
-		clone.querySelector(".discount").textContent = product.price*product.discount/100;
-		clone.querySelector("a").href = "http://kea-alt-del.dk/t5/site/imgs/large/" + product.image + ".jpg";
-		daddy.appendChild(clone);
+		let name = clone.querySelector(".name");
+		let image = clone.querySelector(".product-small-img");
+		let price = clone.querySelector(".price");
+		let discount = clone.querySelector(".discount");
+		let link = clone.querySelector("a");
+		let button = clone.querySelector("button");
+		button.addEventListener("click", function(){
+			console.log(product.id);
+			loadJSON(product_link + product.id, showDetails);
+		})
+		name.textContent = product.name;
+		image.src = "http://kea-alt-del.dk/t5/site/imgs/small/" + product.image + "-sm.jpg";
+		image.alt = product.name;
+		price.textContent = "Price: " + product.price + ",-";
+		if(product.discount > 0){
+			discount.textContent = "Now: " + Math.floor(product.price*product.discount/100) + ",-";
+			price.style.textDecoration="line-through";
+		}else{
+			discount.style.display="none";
+		}
+		link.href = "http://kea-alt-del.dk/t5/site/imgs/large/" + product.image + ".jpg";
+		container.appendChild(clone);
 	});
+}
+
+function showDetails(data){
+	console.log(data);
 }
 
 loadJSON(categories_link, loopCategories);
